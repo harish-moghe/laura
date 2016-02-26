2e823bf54cdd162de997344e4e8c91ebe635f821
Harish Moghe <Harish Moghe>
2016-02-27 00:31:19 +0400

    Added some layouts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,31 +1,37 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Routes File
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you will register all of the routes in an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | This route group applies the "web" middleware group to every route
+  | it contains. The "web" middleware group is defined in your HTTP
+  | kernel and includes session state, CSRF protection, and more.
+  |
+ */
 
 Route::group(['middleware' => ['web']], function () {
     //
+});
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'backend', 'before' => 'admin'], function() {
+    // only /admin/ routes in here that will be in a namespace folder of "backend" with admin middleware
+    Route::resource('pages', 'PagesController'); // app/Http/controllers/backend/PagesController.php
 });
---
+++ resources/views/backend/pages/index.blade.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+@extends('backend.layouts.main')
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Dashboard
+            <small>Control panel</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+    </section><!-- /.content -->
+</div>
+@stop
+
